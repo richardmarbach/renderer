9e69eb0dda2a05f261e52108a1820d7595b9bfc5
Richard Marbach <rmarbach@gmail.com>
2023-11-24 17:54:47 +0100

    Make draw buffer more generic

---
+++ src/draw.zig
@@ -0,0 +1,50 @@
+const std = @import("std");
+
+pub const Buffer = struct {
+    allocator: std.mem.Allocator,
+    buffer: []u32,
+    width: usize,
+    height: usize,
+
+    pub fn init(allocator: std.mem.Allocator, width: usize, height: usize) !Buffer {
+        const buffer = try allocator.alloc(u32, width * height);
+        @memset(buffer, 0xFF000000);
+        return .{ .allocator = allocator, .buffer = buffer, .width = width, .height = height };
+    }
+
+    pub fn deinit(self: *Buffer) void {
+        self.allocator.free(self.buffer);
+    }
+
+    pub inline fn set(self: *Buffer, x: usize, y: usize, color: u32) void {
+        self.buffer[y * self.width + x] = color;
+    }
+
+    pub inline fn fill(self: *Buffer, color: u32) void {
+        @memset(self.buffer, color);
+    }
+};
+
+pub fn grid(buf: *Buffer) void {
+    var y: usize = 0;
+    while (y < buf.height) : (y += 10) {
+        var x: usize = 0;
+        while (x < buf.width) : (x += 10) {
+            buf.set(x, y, 0xFF333333);
+        }
+    }
+}
+
+pub fn rect(buf: *Buffer, x: usize, y: usize, width: usize, height: usize, color: u32) void {
+    const h = @min(y + height, buf.height);
+    const w = @min(x + width, buf.width);
+    for (y..h) |row| {
+        for (x..w) |col| {
+            buf.set(col, row, color);
+        }
+    }
+}
+
+pub fn clear(buf: *Buffer) void {
+    buf.fill(0xFF000000);
+}
--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,6 @@
 const std = @import("std");
 const Display = @import("display.zig").Display;
-const DrawBuffer = @import("draw_buffer.zig").DrawBuffer;
+const draw = @import("draw.zig");
 const c = @cImport({
     @cInclude("SDL2/SDL.h");
 });
@@ -26,9 +26,9 @@ fn process_input(state: *State) void {
     }
 }
 
-fn update(draw_buffer: *DrawBuffer) void {
-    draw_buffer.draw_grid();
-    draw_buffer.draw_rect(200, 300, 50, 100, 0xFFFFFF00);
+fn update(draw_buffer: *draw.Buffer) void {
+    draw.grid(draw_buffer);
+    draw.rect(draw_buffer, 200, 300, 50, 100, 0xFFFFFF00);
 }
 
 pub fn main() !void {
@@ -39,14 +39,14 @@ pub fn main() !void {
     var display = try Display.init();
     defer display.deinit();
 
-    var draw_buffer = try DrawBuffer.init(allocator, display.width, display.height);
+    var draw_buffer = try draw.Buffer.init(allocator, display.width, display.height);
     defer draw_buffer.deinit();
 
     var state = State{};
     while (state.is_running) {
         process_input(&state);
 
-        draw_buffer.clear();
+        draw.clear(&draw_buffer);
         display.clear();
 
         update(&draw_buffer);
