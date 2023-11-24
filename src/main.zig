5395b487b9e8dbb3b2fa54255e87451e531d7882
Richard Marbach <rmarbach@gmail.com>
2023-11-24 15:59:05 +0100

    Grid

--- src/main.zig
+++ src/main.zig
@@ -4,8 +4,8 @@ const c = @cImport({
 });
 
 var isRunning: bool = true;
-var window_width: i32 = 800;
-var window_height: i32 = 600;
+var window_width: u32 = 800;
+var window_height: u32 = 600;
 
 const Window = struct {
     window: *c.SDL_Window,
@@ -23,10 +23,10 @@ const Window = struct {
 
         var displayMode: c.SDL_DisplayMode = undefined;
         _ = c.SDL_GetCurrentDisplayMode(0, &displayMode);
-        window_width = displayMode.w;
-        window_height = displayMode.h;
+        window_width = @bitCast(displayMode.w);
+        window_height = @bitCast(displayMode.h);
 
-        const window = c.SDL_CreateWindow(null, c.SDL_WINDOWPOS_CENTERED, c.SDL_WINDOWPOS_CENTERED, window_width, window_height, c.SDL_WINDOW_BORDERLESS) orelse {
+        const window = c.SDL_CreateWindow(null, c.SDL_WINDOWPOS_CENTERED, c.SDL_WINDOWPOS_CENTERED, @bitCast(window_width), @bitCast(window_height), c.SDL_WINDOW_BORDERLESS) orelse {
             c.SDL_Log("Unable to create window: %s", c.SDL_GetError());
             return error.SDLInitializationFailed;
         };
@@ -43,7 +43,7 @@ const Window = struct {
         return Window{
             .window = window,
             .renderer = renderer,
-            .color_buffer = try allocator.alloc(u32, @as(u32, @bitCast(window_width * window_height))),
+            .color_buffer = try allocator.alloc(u32, window_width * window_height),
             .allocator = allocator,
         };
     }
@@ -77,6 +77,16 @@ pub fn processInput() void {
     }
 }
 
+fn draw_grid(window: *Window) void {
+    var y: usize = 0;
+    while (y < window_height) : (y += 10) {
+        var x: usize = 0;
+        while (x < window_width) : (x += 10) {
+            window.color_buffer[y * window_width + x] = 0xFF333333;
+        }
+    }
+}
+
 pub fn main() !void {
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     defer arena.deinit();
@@ -84,7 +94,7 @@ pub fn main() !void {
     var window = try Window.init(arena.allocator());
     defer window.deinit();
 
-    const color_buffer_texture = c.SDL_CreateTexture(window.renderer, c.SDL_PIXELFORMAT_ARGB8888, c.SDL_TEXTUREACCESS_STREAMING, window_width, window_height) orelse {
+    const color_buffer_texture = c.SDL_CreateTexture(window.renderer, c.SDL_PIXELFORMAT_ARGB8888, c.SDL_TEXTUREACCESS_STREAMING, @bitCast(window_width), @bitCast(window_height)) orelse {
         c.SDL_Log("Unable to create texture: %s", c.SDL_GetError());
         return error.SDLInitializationFailed;
     };
@@ -95,10 +105,11 @@ pub fn main() !void {
         _ = c.SDL_SetRenderDrawColor(window.renderer, 0, 0, 0, 255);
         _ = c.SDL_RenderClear(window.renderer);
 
-        window.clear_color_buffer(0xFFFFFF00);
-
-        _ = c.SDL_UpdateTexture(color_buffer_texture, null, window.color_buffer.ptr, window_width * @sizeOf(u32));
+        draw_grid(&window);
+        _ = c.SDL_UpdateTexture(color_buffer_texture, null, window.color_buffer.ptr, @as(i32, @bitCast(window_width)) * @sizeOf(i32));
         _ = c.SDL_RenderCopy(window.renderer, color_buffer_texture, null, null);
+
+        window.clear_color_buffer(0xFF000000);
 
         c.SDL_RenderPresent(window.renderer);
     }
