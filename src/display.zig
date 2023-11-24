6d77ba560fb03e4e4d1ee63ab1db54fd9d16255c
Richard Marbach <rmarbach@gmail.com>
2023-11-24 17:37:36 +0100

    Refactor

---
+++ src/display.zig
@@ -0,0 +1,65 @@
+const c = @cImport({
+    @cInclude("SDL2/SDL.h");
+});
+
+pub const Display = struct {
+    width: u32,
+    height: u32,
+    window: *c.SDL_Window,
+    renderer: *c.SDL_Renderer,
+    buffer_texture: *c.SDL_Texture,
+
+    pub fn init() !Display {
+        if (c.SDL_Init(c.SDL_INIT_EVERYTHING) != 0) {
+            c.SDL_Log("Unable to initialize SDL: %s", c.SDL_GetError());
+            return error.SDLInitializationFailed;
+        }
+        errdefer c.SDL_Quit();
+
+        var displayMode: c.SDL_DisplayMode = undefined;
+        _ = c.SDL_GetCurrentDisplayMode(0, &displayMode);
+        const window_width: u32 = @bitCast(displayMode.w);
+        const window_height: u32 = @bitCast(displayMode.h);
+
+        const window = c.SDL_CreateWindow(null, c.SDL_WINDOWPOS_CENTERED, c.SDL_WINDOWPOS_CENTERED, @bitCast(window_width), @bitCast(window_height), c.SDL_WINDOW_BORDERLESS) orelse {
+            c.SDL_Log("Unable to create window: %s", c.SDL_GetError());
+            return error.SDLInitializationFailed;
+        };
+        errdefer c.SDL_DestroyWindow(window);
+
+        const renderer = c.SDL_CreateRenderer(window, -1, 0) orelse {
+            c.SDL_Log("Unable to create renderer: %s", c.SDL_GetError());
+            return error.SDLInitializationFailed;
+        };
+        errdefer c.SDL_DestroyRenderer(renderer);
+
+        _ = c.SDL_SetWindowFullscreen(window, c.SDL_WINDOW_FULLSCREEN);
+
+        const buffer_texture = c.SDL_CreateTexture(renderer, c.SDL_PIXELFORMAT_ARGB8888, c.SDL_TEXTUREACCESS_STREAMING, @bitCast(window_width), @bitCast(window_height)) orelse {
+            c.SDL_Log("Unable to create texture: %s", c.SDL_GetError());
+            return error.SDLInitializationFailed;
+        };
+
+        return Display{ .window = window, .renderer = renderer, .buffer_texture = buffer_texture, .width = window_width, .height = window_height };
+    }
+
+    pub fn deinit(self: *Display) void {
+        c.SDL_DestroyRenderer(self.renderer);
+        c.SDL_DestroyWindow(self.window);
+        c.SDL_Quit();
+    }
+
+    pub fn clear(self: *Display) void {
+        _ = c.SDL_SetRenderDrawColor(self.renderer, 0, 0, 0, 255);
+        _ = c.SDL_RenderClear(self.renderer);
+    }
+
+    pub fn draw_buffer(self: *Display, buffer: []u32) void {
+        _ = c.SDL_UpdateTexture(self.buffer_texture, null, buffer.ptr, @bitCast(self.width * @sizeOf(i32)));
+        _ = c.SDL_RenderCopy(self.renderer, self.buffer_texture, null, null);
+    }
+
+    pub fn render(self: *Display) void {
+        c.SDL_RenderPresent(self.renderer);
+    }
+};
--- src/main.zig
+++ src/main.zig
@@ -1,64 +1,11 @@
 const std = @import("std");
+const Display = @import("display.zig").Display;
+const DrawBuffer = @import("draw_buffer.zig").DrawBuffer;
 const c = @cImport({
     @cInclude("SDL2/SDL.h");
 });
 
 var isRunning: bool = true;
-var window_width: u32 = 800;
-var window_height: u32 = 600;
-
-const Window = struct {
-    window: *c.SDL_Window,
-    renderer: *c.SDL_Renderer,
-
-    allocator: std.mem.Allocator,
-    color_buffer: []u32,
-
-    pub fn init(allocator: std.mem.Allocator) !Window {
-        if (c.SDL_Init(c.SDL_INIT_EVERYTHING) != 0) {
-            c.SDL_Log("Unable to initialize SDL: %s", c.SDL_GetError());
-            return error.SDLInitializationFailed;
-        }
-        errdefer c.SDL_Quit();
-
-        var displayMode: c.SDL_DisplayMode = undefined;
-        _ = c.SDL_GetCurrentDisplayMode(0, &displayMode);
-        window_width = @bitCast(displayMode.w);
-        window_height = @bitCast(displayMode.h);
-
-        const window = c.SDL_CreateWindow(null, c.SDL_WINDOWPOS_CENTERED, c.SDL_WINDOWPOS_CENTERED, @bitCast(window_width), @bitCast(window_height), c.SDL_WINDOW_BORDERLESS) orelse {
-            c.SDL_Log("Unable to create window: %s", c.SDL_GetError());
-            return error.SDLInitializationFailed;
-        };
-        errdefer c.SDL_DestroyWindow(window);
-
-        const renderer = c.SDL_CreateRenderer(window, -1, 0) orelse {
-            c.SDL_Log("Unable to create renderer: %s", c.SDL_GetError());
-            return error.SDLInitializationFailed;
-        };
-        errdefer c.SDL_DestroyRenderer(renderer);
-
-        _ = c.SDL_SetWindowFullscreen(window, c.SDL_WINDOW_FULLSCREEN);
-
-        return Window{
-            .window = window,
-            .renderer = renderer,
-            .color_buffer = try allocator.alloc(u32, window_width * window_height),
-            .allocator = allocator,
-        };
-    }
-
-    pub fn clear_color_buffer(self: *Window, color: u32) void {
-        @memset(self.color_buffer, color);
-    }
-
-    pub fn deinit(self: *Window) void {
-        self.allocator.free(self.color_buffer);
-        c.SDL_DestroyRenderer(self.renderer);
-        c.SDL_DestroyWindow(self.window);
-        c.SDL_Quit();
-    }
-};
 
 pub fn processInput() void {
     var event: c.SDL_Event = undefined;
@@ -77,51 +24,28 @@ pub fn processInput() void {
     }
 }
 
-fn draw_grid(window: *Window) void {
-    var y: usize = 0;
-    while (y < window_height) : (y += 10) {
-        var x: usize = 0;
-        while (x < window_width) : (x += 10) {
-            window.color_buffer[y * window_width + x] = 0xFF333333;
-        }
-    }
-}
-
-fn draw_rect(window: *Window, x: usize, y: usize, width: usize, height: usize, color: u32) void {
-    const h = @min(y + height, window_height);
-    const w = @min(x + width, window_width);
-    for (y..h) |row| {
-        for (x..w) |col| {
-            window.color_buffer[row * window_width + col] = color;
-        }
-    }
-}
-
 pub fn main() !void {
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     defer arena.deinit();
+    const allocator = arena.allocator();
 
-    var window = try Window.init(arena.allocator());
-    defer window.deinit();
+    var display = try Display.init();
+    defer display.deinit();
 
-    const color_buffer_texture = c.SDL_CreateTexture(window.renderer, c.SDL_PIXELFORMAT_ARGB8888, c.SDL_TEXTUREACCESS_STREAMING, @bitCast(window_width), @bitCast(window_height)) orelse {
-        c.SDL_Log("Unable to create texture: %s", c.SDL_GetError());
-        return error.SDLInitializationFailed;
-    };
+    var draw_buffer = try DrawBuffer.init(allocator, display.width, display.height);
+    defer draw_buffer.deinit();
 
     while (isRunning) {
         processInput();
 
-        _ = c.SDL_SetRenderDrawColor(window.renderer, 0, 0, 0, 255);
-        _ = c.SDL_RenderClear(window.renderer);
+        display.clear();
 
-        draw_grid(&window);
-        draw_rect(&window, 200, 300, 50, 100, 0xFFFFFF00);
-        _ = c.SDL_UpdateTexture(color_buffer_texture, null, window.color_buffer.ptr, @as(i32, @bitCast(window_width)) * @sizeOf(i32));
-        _ = c.SDL_RenderCopy(window.renderer, color_buffer_texture, null, null);
+        draw_buffer.draw_grid();
+        draw_buffer.draw_rect(200, 300, 50, 100, 0xFFFFFF00);
 
-        window.clear_color_buffer(0xFF000000);
+        display.draw_buffer(draw_buffer.buffer);
+        draw_buffer.clear();
 
-        c.SDL_RenderPresent(window.renderer);
+        display.render();
     }
 }
