c9efac7d89dcd5d1a1481fe9f69bdcfad2aeadd2
Richard Marbach <rmarbach@gmail.com>
2023-11-26 13:58:56 +0100

    Back by Simd Type

--- src/vec.zig
+++ src/vec.zig
@@ -1,18 +1,38 @@
 pub fn Vec2(comptime T: type) type {
-    // const Self = @Type();
+    const SimdVec2 = @Vector(2, T);
 
-    return struct {
-        x: T,
+    const Self = @This();
+
+    return extern struct {
+        x: T align(@alignOf(SimdVec2)),
         y: T,
+
+        fn fromSimd(simd: SimdVec2) Self {
+            return @as(Self, @bitCast(simd));
+        }
+
+        fn toSimd(self: Self) SimdVec2 {
+            return @as(SimdVec2, @bitCast(self));
+        }
     };
 }
 
 pub fn Vec3(comptime T: type) type {
-    // const Self = @Type();
+    const SimdVec3 = @Vector(3, T);
 
-    return struct {
-        x: T,
+    const Self = @This();
+
+    return extern struct {
+        x: T align(@alignOf(SimdVec3)),
         y: T,
         z: T,
+
+        fn fromSimd(simd: SimdVec3) Self {
+            return @as(Self, @bitCast(simd));
+        }
+
+        fn toSimd(self: Self) SimdVec3 {
+            return @as(SimdVec3, @bitCast(self));
+        }
     };
 }
